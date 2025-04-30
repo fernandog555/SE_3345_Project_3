0496b680c9028aa2b633759f94dfb2985ed51aba
fernandog555 <44325716+fernandog555@users.noreply.github.com>
2025-04-30 18:21:21 -0500

    Create QuickSorter.java

---
+++ src/QuickSorter.java
@@ -0,0 +1,17 @@
+public class QuickSorter extends <Comparable <T>>
+{
+    private enum PivotStrategy
+    {
+        FIRST_ELEMENT,
+        RANDOM_ELEMENT,
+        MEDIAN_OF_TWO_ELEMENTS,
+        MEDIAN_OF_THREE_ELEMENTS
+    }
+
+    // Use for enum: PivotStrategy myVar = PivotStrategy.FIRST_ELEMENT;
+
+    static timeedQuickSort<E extends Comparable<E>>(list ArrayList<E>)
+    {
+
+    }
+}
