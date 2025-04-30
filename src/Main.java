7d069f7109f418930bfb6ba80a10b21fe2e7b9fb
fernandog555 <44325716+fernandog555@users.noreply.github.com>
2025-04-30 18:19:55 -0500

    base main logic

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
+import java.io.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Need to 4 command line arguments
+        // Will read in a file, and output to a file
+
+        // Check if command line arguments were provided. Otherwise do nothing and give error to user.
+        if (args.length != 4)
+        {
+            System.out.println("Warning: It looks like there is no arguments provided. Please check your arguments.");
+            System.out.println("Usage: <ARRAY_SIZE> <REPORTS_FILE> <UNSORTED_FILE> <SORTED_FILE>");
+            return;
         }
+
+        // Assuming we got the files opened and read.
+        System.out.println("Arguments were successfully provided. Check the files for results.");
+
+        int arraySize = args[0];
+        String reportFile = args[1];
+        String unsortedFile = args[2];
+        String sortedFile = args[3];
+
+        // Try catch block for reading and writing the files
+        try (Scanner scanFile = new Scanner(new File(inputFile));
+             PrintWriter writer = new PrintWriter(new FileWriter(outputFile)))
+        {
+        // Scan each line of the input file and process those commands until we reach end of file.
+            while (scanFile.hasNextLine())
+            {
+
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Command file not found: " + inputFile);
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error writing to output file: " + outputFile);
+        }
+
     }
 }
